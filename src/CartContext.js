32735edfd5461b8ffb5091867c1a63f355b9022f
mqasim5171 <mqasim5171@gmail.com>
2025-02-05 20:08:40 +0500

    last commit

--- src/CartContext.js
+++ src/CartContext.js
@@ -5,12 +5,32 @@ export const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
+  
   const addToCart = (book) => {
-    setCart((prevCart) => [...prevCart, book]);
+    setCart((prevCart) => {
+      const existingBook = prevCart.find((item) => item.id === book.id);
+      if (existingBook) {
+        return prevCart.map((item) =>
+          item.id === book.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      }
+      return [...prevCart, { ...book, price: parseFloat(book.price.replace("$", "")), quantity: 1 }];
+    });
+  };
+  
+
+  const removeFromCart = (bookId) => {
+    setCart((prevCart) =>
+      prevCart
+        .map((item) =>
+          item.id === bookId ? { ...item, quantity: item.quantity - 1 } : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
       {children}
     </CartContext.Provider>
   );
